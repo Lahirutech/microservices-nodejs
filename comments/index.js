de77db65666f1f5471ece6744f522aa8990f591f
Lanka Rathnayaka <rmllbrathnayaka@gmail.com>
2024-11-22 11:21:12 +1100

    add receiving and emiting events

--- comments/index.js
+++ comments/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
-
+const { type } = require("os");
+const axios = require("axios");
 const app = express();
 
 app.use(bodyParser.json());
@@ -14,7 +15,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const comentId = randomBytes(4).toString("hex");
   const { content } = req.body;
 
@@ -23,7 +24,25 @@ app.post("/posts/:id/comments", (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
+  await axios
+    .post("http://localhost:4005/events", {
+      type: "CommentCreated",
+      data: {
+        id: comentId,
+        content,
+        postId: req.params.id,
+      },
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Event Received", req.body.type);
+  res.send({});
 });
 
 app.listen(4001, () => {

c24843de2d315e6340fc5df29e9a1d3e5047a088
Lanka <lanka@symbiote.com.au>
2024-12-13 12:59:25 +1100

    adding the moderation service

--- query/index.js
+++ query/index.js
@@ -26,6 +26,17 @@ app.post("/events", (req, res) => {
     const post = posts[postId];
     post.comments.push({ id, content });
   }
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
   console.log(posts);
   res.send({});
 });

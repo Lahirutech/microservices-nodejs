18946eef2bd31841f75da8be6e8684c2a906a5fa
Lanka <lanka@symbiote.com.au>
2024-12-13 14:22:08 +1100

    add event bus storage

--- query/index.js
+++ query/index.js
@@ -13,19 +13,20 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
-
-  if (type == "PostCreated") {
+const handleEvent = (type, data) => {
+  if (type === "PostCreated") {
     const { id, title } = data;
+
     posts[id] = { id, title, comments: [] };
   }
+
   if (type === "CommentCreated") {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
 
     const post = posts[postId];
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
   }
+
   if (type === "CommentUpdated") {
     const { id, content, postId, status } = data;
 
@@ -37,10 +38,24 @@ app.post("/events", (req, res) => {
     comment.status = status;
     comment.content = content;
   }
-  console.log(posts);
+};
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("Listen on 4002");
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+    for (let event of res.data) {
+      console.log("Processing event", event.type);
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
